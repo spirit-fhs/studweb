ab3cd14da560ce74a3a636280f47eeb743574680
Florian Schuhmann <schuhmann.florian@googlemail.com>
2011-03-05 20:22:39 +0100

    change db connection to SQLite; add login, auth, acl functionality; added a db loading script

---
+++ scripts/data.sqlite.sql
@@ -0,0 +1,7 @@
+INSERT INTO entry (cruser, subject, text, crdate) VALUES
+('KUENZEL', 'ITIL-Blockseminar', 'Das ITIL-Blockseminar bei Herrn Winkelmann findet am 25.02.11 ab 8:15 Uhr im PC-Pool1 (B0107) statt.', DATETIME('NOW'));
+INSERT INTO entry (cruser, subject, text, crdate) VALUES
+('KUENZEL2', 'ITIL', 'Herrn Winkelmann findet am 25.02.11 ab 8:15 Uhr im PC-Pool1 (B0107) statt.', DATETIME('NOW'));
+INSERT INTO entry (cruser, subject, text, crdate) VALUES
+('KUENZEL2', 'ITIL', 'Herrn Winkelmann findet am 25.02.11 ab 8:15 Uhr im PC-Pool1 (B0107) statt.', DATETIME('NOW'));
+INSERT INTO users ('username', 'password', 'role') VALUES ('Admin', 'geheim', 'admin');
